c300be3ed99c2d6eaf0dff639c97fc1485c4cc6a
HeadSplit <eualku2@gmail.com>
2025-05-21 16:51:06 +0500

    feat(frontend): added styles

--- backend/DjangoProject/urls.py
+++ backend/DjangoProject/urls.py
@@ -21,7 +21,10 @@ from todo import views as todo_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='login.html',
+        redirect_authenticated_user=True
+    ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', todo_views.register, name='register'),
     path('', include('todo.urls')),
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
